7e17e02f130605ebafbf1cce659655613966c584
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2024-10-05 09:34:25 +0530

    dev : updated Newsletter component and footer to be responsive for mobile screens

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,21 @@
-import React from 'react'
-import footer_logo from './assets/logo_big.png'
-import insticon from './assets/instagram_icon.png'
-import pintrest from './assets/pintester_icon.png'
-import whatsapp from './assets/whatsapp_icon.png'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import footer_logo from './assets/logo_big.png';
+import insticon from './assets/instagram_icon.png';
+import pintrest from './assets/pintester_icon.png';
+import whatsapp from './assets/whatsapp_icon.png';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div className='flex flex-col justify-center items-center gap-[50px] pt-[100px]'>
-
-
+    <div className='flex flex-col justify-center items-center gap-[50px] pt-[100px] px-4 md:px-0'>
       <Link to='/'>
-      <div className='flex items-center gap-[20px] cursor-pointer '>
-        <img className='' src={footer_logo} alt="" />
-        <p className='text-[#383838] text-[46px] font-bold'>Shopper's Stop</p>
-      </div>
+        <div className='flex items-center gap-[20px] cursor-pointer'>
+          <img className='h-12 w-auto' src={footer_logo} alt="Shopper's Stop Logo" />
+          <p className='text-[#383838] text-[36px] md:text-[46px] font-bold'>Shopper's Stop</p>
+        </div>
       </Link>
 
-
-      <ul className='footer-links flex gap-[50px] text-[#252525] list-none text-[20px] '>
+      <ul className='footer-links flex flex-wrap justify-center gap-[20px] text-[#252525] list-none text-[16px] md:text-[20px]'>
         <li className='cursor-pointer'>Company</li>
         <li className='cursor-pointer'>Products</li>
         <li className='cursor-pointer'>Offices</li>
@@ -26,29 +23,25 @@ const Footer = () => {
         <li className='cursor-pointer'>Contact</li>
       </ul>
 
-
-      <div className='flex  gap-[20px] '>
-
-        <div className='p-2.5 pb-[6px]  cursor-pointer'>
-            <img src={insticon} alt="" />
+      <div className='flex gap-[20px]'>
+        <div className='p-2 cursor-pointer'>
+          <img className='h-8 w-auto' src={insticon} alt="Instagram" />
         </div>
-
-        <div className='p-2.5 pb-[6px] cursor-pointer'>
-            <img src={pintrest} alt="" />
+        <div className='p-2 cursor-pointer'>
+          <img className='h-8 w-auto' src={pintrest} alt="Pinterest" />
         </div>
-
-        <div className='p-2.5 pb-[6px]  cursor-pointer'>
-            <img src={whatsapp} alt="" />
+        <div className='p-2 cursor-pointer'>
+          <img className='h-8 w-auto' src={whatsapp} alt="WhatsApp" />
         </div>
       </div>
 
-      <div className='flex flex-col items-center gap-[30px] w-[100%] mb-[30px] text-[#1a1a1a] text-[20px] '>
-      <div className="w-[80%] h-[3px] bg-[#57697b99] mt-[25px] "></div>
-        <hr className='w-[80%] border-none rounded-[10px] h-[3px] text-[#000000cc]'/>
-        <p>Copyright @ 2024 - All Right Reserved</p>
+      <div className='flex flex-col items-center gap-[20px] w-full mb-[30px] text-[#1a1a1a] text-[16px] md:text-[20px]'>
+        <div className="w-[80%] h-[3px] bg-[#57697b99] mt-[25px]" />
+        <hr className='w-[80%] border-none rounded-[10px] h-[3px] text-[#000000cc]' />
+        <p>Copyright @ 2024 - All Rights Reserved</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const Newsletter = () => {
   return (
-    <div className='w-[75%] h-[70vh] flex flex-col justify-center items-center gap-[30px] m-auto pt-0 pb-0 pr-[140px] pl-[140px] mb-[150px] bg-gradient-to-r from-[#91A6FF] to-[#FFFFFF] '>
-      <h1 className='text-[#454545] text-[55px] font-bold'>Get Exclusive Offers on Your Email</h1>
-      <p className='text-[#454545] text-[22px]'>Subscribe our Newsletter to stay Updated</p>
+    <div className='w-full max-w-[1200px] h-auto flex flex-col justify-center items-center gap-[30px] m-auto p-6 md:p-10 mb-[150px] bg-gradient-to-r from-[#91A6FF] to-[#FFFFFF]'>
+      <h1 className='text-[#454545] text-[36px] md:text-[55px] font-bold text-center'>Get Exclusive Offers on Your Email</h1>
+      <p className='text-[#454545] text-[16px] md:text-[22px] text-center'>Subscribe to our Newsletter to stay updated</p>
 
-      <div className='flex items-center justify-space-between bg-white w-[730px] h-[70px] rounded-[80px] border-[1px] border-[#e3e3e3]'>
-        <input className='w-[500px] ml-[30px] border-0 text-[#616161] font-poppins text-[16px] outline-none' type="email" placeholder='Enter Your Email'/>
-        <button className='w-[210px] h-[70px] bg-black text-white text-[16px] cursor-pointer rounded-[80px]'>Subscribe</button>
+      <div className='flex flex-col md:flex-row items-center justify-between bg-white w-full md:w-[730px] h-[70px] rounded-[80px] border-[1px] border-[#e3e3e3]'>
+        <input className='w-full md:w-[500px] ml-4 md:ml-[30px] border-0 text-[#616161] font-poppins text-[16px] outline-none h-full p-2' type="email" placeholder='Enter Your Email'/>
+        <button className='w-full md:w-[210px] h-[70px] bg-black text-white text-[16px] cursor-pointer rounded-[80px] mt-2 md:mt-0'>Subscribe</button>
       </div>
     </div>
-  )
+  );
 }
 
-export default Newsletter
+export default Newsletter;
