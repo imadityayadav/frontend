c1ca5f50e15f06dff9e3eb97ee7217bc4fd49873
Diar Fonda <Diarfondaa@gmail.com>
2024-10-05 12:00:53 +0200

    ui updates; bugs & improvements

--- src/components/Breadcrum.jsx
+++ src/components/Breadcrum.jsx
@@ -1,17 +1,16 @@
+// import React from 'react';
+// import arrow from './assets/breadcrum_arrow.png';
+// import './card.css';
 
-import React from 'react';
-import arrow from './assets/breadcrum_arrow.png';
-import './card.css';
+// const Breadcrum = (props) => {
+//   const { product } = props;
 
-const Breadcrum = (props) => {
-  const { product } = props;
-
-  return (
-    <div className='flex items-center gap-[8px] text-[#5e5e5e] text-[16px] font-bold my-[60px] mx-[170px] capitalize'>
-      HOME <img src={arrow} alt="" /> SHOP <img src={arrow} alt="" /> {product.category} <img src={arrow} alt="" /> {product.name}
-    </div>
-  );
-}
+//   return (
+//     <div className='flex items-center gap-[8px] text-[#5e5e5e] text-[16px] font-bold my-[60px] mx-[170px] capitalize'>
+//       HOME <img src={arrow} alt="" /> SHOP <img src={arrow} alt="" /> {product.category} <img src={arrow} alt="" /> {product.name}
+//     </div>
+//   );
+// }
 
 import React from "react";
 import arrow from "./assets/breadcrum_arrow.png";
@@ -22,22 +21,31 @@ const Breadcrum = (props) => {
   const navigate = useNavigate();
   const breadCrumbHandler = (path) => {
     navigate(path);
-  }
+  };
 
   return (
     <div className="flex items-center gap-[8px] text-[#5e5e5e] text-[16px] font-bold my-[60px] mx-[170px] capitalize">
-      <div className="cursor-pointer hover:text-[#EB423F]" onClick={()=>{breadCrumbHandler("/")}}>
-        Home 
+      <div
+        className="cursor-pointer hover:text-[#EB423F]"
+        onClick={() => {
+          breadCrumbHandler("/");
+        }}
+      >
+        Home
       </div>
       <img src={arrow} alt="" />
-      <div className="cursor-pointer hover:text-[#EB423F]" onClick={()=>{breadCrumbHandler(`/${product.category.toLowerCase()}`)}}>
-        {product.category} Shop 
-      </div> 
-      <img src={arrow} alt="" /> 
+      <div
+        className="cursor-pointer hover:text-[#EB423F]"
+        onClick={() => {
+          breadCrumbHandler(`/${product.category.toLowerCase()}`);
+        }}
+      >
+        {product.category} Shop
+      </div>
+      <img src={arrow} alt="" />
       {product.name}
     </div>
   );
 };
-
 
 export default Breadcrum;
--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
-import eximg from './assets/girl.webp';
+import React from "react";
+import eximg from "./assets/girl.webp";
 
 const Offers = () => {
   return (
-    <div className='offers w-full h-auto flex flex-col md:flex-row mt-12 mx-0 mb-36 bg-gradient-to-r from-[#5A585A] to-[#090947] p-5 md:p-20'>
-      <div className='offer-left flex flex-1 flex-col justify-center'>
-        <h1 className='text-[#171717] text-5xl md:text-[80px] font-bold'>Exclusive</h1>
-        <h1 className='text-[#171717] text-xl md:text-[22px] font-bold'>Offers for you</h1>
-        <p className='text-base'>ONLY ON BEST SELLER PRODUCTS</p>
-        <button className='w-[282px] h-[70px] rounded-full bg-[#ff4141] text-white text-xl mt-6 cursor-pointer'>
+    <div className="offers w-full h-auto flex flex-col md:flex-row mt-12 mx-0 mb-36 bg-gradient-to-r from-[#5A585A] to-[#090947] p-5 md:p-20">
+      <div className="offer-left flex flex-1 flex-col justify-center">
+        <h1 className="text-[#171717] text-5xl md:text-[80px] font-bold">
+          Exclusive
+        </h1>
+        <h1 className="text-[#171717] text-xl md:text-[22px] font-bold">
+          Offers for you
+        </h1>
+        <p className="text-base">ONLY ON BEST SELLER PRODUCTS</p>
+        <button className="w-[282px] h-[70px] rounded-full bg-[#ff4141] hover:bg-[#c22e2e] transition-all text-white text-xl mt-6 cursor-pointer">
           Check Now
         </button>
       </div>
 
-      <div className='flex justify-center mt-8 md:mt-0'>
-        <img className='w-full h-auto max-w-xs md:max-w-sm' src={eximg} alt="Exclusive Offers" />
+      <div className="flex justify-center mt-8 md:mt-0">
+        <img
+          className="w-full h-auto max-w-xs md:max-w-sm"
+          src={eximg}
+          alt="Exclusive Offers"
+        />
       </div>
     </div>
   );
