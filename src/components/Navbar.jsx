ac939cc3672cbc0d68edf2ab9e4ab89e57f6ce05
udcash <uttkarshthakur0326@gmail.com>
2024-10-05 12:36:54 +0530

    hover effect on navbar items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,44 +12,71 @@ const Navbar = () => {
     const { getTotalCartItems } = useContext(ShopContext);
     const menuRef = useRef();
 
-    const toggle = (e)=>{
+    const toggle = (e) => {
         menuRef.current.classlist.toggle('nav-menu-visible');
         e.target.classlist.toggle('open');
     }
-    
+
     return (
-        <div className='navbar flex justify-around p-[25px] shadow-md items-center bg-white max-[1280px]:py-[12px] max-[1280px]:px-[50px] max-[1024px]:py-[12px] max-[1024px]:px-[30px] max-[800px]:py-[10px] max-[800px]:px-0 '>
+        <div className='navbar flex justify-around p-[25px] shadow-md items-center bg-white max-[1280px]:py-[12px] 
+        max-[1280px]:px-[50px] max-[1024px]:py-[12px] max-[1024px]:px-[30px] max-[800px]:py-[10px] max-[800px]:px-0 '>
             <div className='flex items-center gap-[10px]'>
                 <img className='xl:w-[40px] max-[800px]:' src={logo} alt="" />
                 <p className='text-black text-[40px] font-semibold max-[1280px]:text-[25px]'>Shopper's Stop</p>
             </div>
             <img className='hidden max-[800px]:block max-[800px]:w-[30px]' src={ham} onClick={toggle} alt="" />
-            
-            <ul ref={menuRef} className='flex text-center justify-center gap-[50px] text-[20px] font-medium text-[#626262] cursor-pointer max-[1280px]:gap-[30px] max-[1280px]:text-[16px] max-[1024px]:gap-[25px] max-[1024px]:text-[14px] max-[800px]:flex max-[800px]:h-[80px] max-[800px]:w-[100%] max-[800px]:absolute max-[800px]:bg-white max-[800px]:justify-center max-[800px]:top-[70px]  '>
-                <li onClick={() => { setMenu("shop") }}> <Link to="/">Shop</Link>{menu === "shop" ? <hr className='w-[80%] h-[3px] rounded-sm bg-[#FF4141]' /> : <></>}
 
+            <ul ref={menuRef} className='flex text-center justify-center gap-[50px] text-[20px] 
+                             font-medium text-[#626262] cursor-pointer max-[1280px]:gap-[30px] max-[1280px]:text-[16px]
+                             max-[1024px]:gap-[25px] max-[1024px]:text-[14px] max-[800px]:flex max-[800px]:h-[80px] max-[800px]:w-[100%]
+                             max-[800px]:absolute max-[800px]:bg-white max-[800px]:justify-center max-[800px]:top-[70px]'>
+
+                <li onClick={() => { setMenu("shop") }} className="group relative px-3 py-1 rounded-[1rem]">
+                    <div className="absolute inset-0 bg-gray-200 rounded-full transform scale-0 
+                    transition-transform duration-500 ease-out group-hover:scale-150 origin-center"></div>
+                    <Link to="/" className="relative z-10 hover:text-black transition-all duration-300">
+                        Shop
+                    </Link>
+                    {menu === "shop" ? <hr className='w-[80%] h-[3px] rounded-sm bg-[#FF4141]' /> : <></>}
                 </li>
-                <li  onClick={() => { setMenu("men") }}><Link to="/men">Men</Link>{menu === "men" ? <hr className='w-[80%] h-[3px] rounded-sm bg-[#FF4141]' /> : <></>}</li>
-                <li onClick={() => { setMenu("kids") }}><Link to="/kids">Kids</Link>{menu === "kids" ? <hr className='w-[80%] h-[3px] rounded-sm bg-[#FF4141]' /> : <></>}</li>
-                <li onClick={() => { setMenu("women") }}><Link to="/women">Women</Link>{menu === "women" ? <hr className='w-[80%] h-[3px] rounded-sm bg-[#FF4141]' /> : <></>}</li>
+                <li onClick={() => { setMenu("men") }} className="group relative px-3 py-1 rounded-[1rem]">
+                    <div className="absolute inset-0 bg-gray-200 rounded-full transform scale-0 
+                    transition-transform duration-500 ease-out group-hover:scale-150 origin-center"></div>
+                    <Link to="/men" className="relative z-10 hover:text-black transition-all duration-300">
+                        Men
+                    </Link>
+                    {menu === "men" ? <hr className='w-[80%] h-[3px] rounded-sm bg-[#FF4141]' /> : <></>}
+                </li>
+                <li onClick={() => { setMenu("kids") }} className="group relative px-3 py-1 rounded-[1rem]">
+                    <div className="absolute inset-0 bg-gray-200 rounded-full transform scale-0 
+                    transition-transform duration-500 ease-out group-hover:scale-150 origin-center"></div>
+                    <Link to="/kids" className="relative z-10 hover:text-black transition-all duration-300">
+                        Kids
+                    </Link>
+                    {menu === "kids" ? <hr className='w-[80%] h-[3px] rounded-sm bg-[#FF4141]' /> : <></>}
+                </li>
+                <li onClick={() => { setMenu("women") }} className="group relative px-3 py-1 rounded-[1rem]">
+                    <div className="absolute inset-0 bg-gray-200 rounded-full transform scale-0 
+                    transition-transform duration-500 ease-out group-hover:scale-150 origin-center"></div>
+                    <Link to="/women" className="relative z-10 hover:text-black transition-all duration-300">
+                        Women
+                    </Link>
+                    {menu === "women" ? <hr className='w-[80%] h-[3px] rounded-sm bg-[#FF4141]' /> : <></>}
+                </li>
             </ul>
             <div className='flex items-center gap-[45px] max-[1280px]:gap-[30px]'>
-
-
                 <Link to='/login'>
-                <button className='px-4 py-2 bg-transparent text-black font-semibold rounded-lg border border-gray-400 hover:bg-black hover:text-white transition duration-300 ease-in-out'>Login</button>
+                    <button className='px-4 py-2 bg-transparent text-black font-semibold rounded-lg border border-gray-400 hover:bg-black hover:text-white transition duration-300 ease-in-out'>
+                        Login
+                    </button>
                 </Link>
-
-
-                <Link to='/cart'><img className='max-[1024px]:w-[30px]' src={cart} alt="" /></Link>
-                <div className='w-[22px] h-[22px] flex justify-center items-center mt-[-35px] ml-[-55px] text-[11px] rounded-full bg-[#FF4141] text-white max-[1280px]:ml-[-40px] max-[1280px]:text-[12px] max-[1024px]:w-[18px] max-[1024px]:h-[18px]'>{getTotalCartItems()}</div>
-
-
-
-
+                <Link to='/cart'>
+                    <img className='max-[1024px]:w-[30px]' src={cart} alt="" /></Link>
+                <div className='w-[22px] h-[22px] flex justify-center items-center mt-[-35px] ml-[-55px] text-[11px] rounded-full bg-[#FF4141] text-white max-[1280px]:ml-[-40px] max-[1280px]:text-[12px] max-[1024px]:w-[18px] max-[1024px]:h-[18px]'>
+                    {getTotalCartItems()}
+                </div>
             </div>
         </div>
     )
 }
-
 export default Navbar
