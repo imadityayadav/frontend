3929add13b1e6558a699a577b43c6afa8dbfeb64
cynthia <cynthiaawsajira@gmail.com>
2024-10-05 17:30:26 +0300

    [style]: update text color to white in Offers component

--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -5,9 +5,9 @@ const Offers = () => {
   return (
     <div className='offers w-full h-auto flex flex-col md:flex-row mt-12 mx-0 mb-36 bg-gradient-to-r from-[#5A585A] to-[#090947] p-5 md:p-20'>
       <div className='offer-left flex flex-1 flex-col justify-center'>
-        <h1 className='text-[#171717] text-5xl md:text-[80px] font-bold'>Exclusive</h1>
-        <h1 className='text-[#171717] text-xl md:text-[22px] font-bold'>Offers for you</h1>
-        <p className='text-base'>ONLY ON BEST SELLER PRODUCTS</p>
+        <h1 className='text-[#ffffff] text-5xl md:text-[80px] font-bold'>Exclusive</h1>
+        <h1 className='text-[#ffffff] text-xl md:text-[22px] font-bold'>Offers for you</h1>
+        <p className='text-[#ffffff]'>ONLY ON BEST SELLER PRODUCTS</p>
         <button className='w-[282px] h-[70px] rounded-full bg-[#ff4141] text-white text-xl mt-6 cursor-pointer'>
           Check Now
         </button>
