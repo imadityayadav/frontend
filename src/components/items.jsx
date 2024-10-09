69a0863c55c86a44723f2072653e49d44a3a8a7e
Snehal <meshramsnehal9356@gmail.com>
2024-10-10 02:14:04 +0530

    Added star rating system overlay on product images.

--- src/components/items.jsx
+++ src/components/items.jsx
@@ -1,30 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import star from './assets/star_icon.png'; 
+import stardull from './assets/star_dull_icon.png'; 
 
-const Items = (props) => {
+const Item = (props) => {
     return (
-        <div className='w-[280px] hover:scale-105 transition duration-150 hover:shadow-2xl max-[1280px]:w-[220px] max-[1280px]:text-[14px] max-[1024px]:w-[170px] max-[1024px]:text-[13px] max-[800px]:w-[120px] max-[800px]:text-[12px] max-[500px]:w-[160px]'>
+        <div className='w-[280px] hover:scale-105 transition duration-150 hover:shadow-2xl relative'>
             <Link to={`/product/${props.id}`} onClick={() => window.scrollTo(0, 0)}>
-                <img className='w-full h-auto max-[1280px]:w-full max-[1024px]:w-full max-[800px]:w-full max-[500px]:w-full' src={props.image} alt={props.name} />
+                <img className='w-full h-auto' src={props.image} alt={props.name} />
             </Link>
             <p className='mt-2 mb-2 text-center'>{props.name}</p>
             <div className='item-prices flex justify-between items-center'>
-                <div className='item-price-new text-[#374151] text-[18px] font-bold max-[1280px]:text-[14px] max-[1024px]:text-[13px] max-[800px]:text-[12px]'>
+                <div className='item-price-new text-[#374151] text-[18px] font-bold'>
                     ${props.new_price}
                 </div>
-                <div className='items-price-old text-[#8c8c8c] text-[18px] font-semibold line-through max-[1280px]:text-[14px] max-[1024px]:text-[13px] max-[800px]:text-[12px]'>
+                <div className='items-price-old text-[#8c8c8c] text-[18px] font-semibold line-through'>
                     ${props.old_price}
                 </div>
             </div>
+
+            <div className='flex items-center justify-start gap-1 mt-2'>
+                {Array.from({ length: 5 }, (_, index) => (
+                    <img
+                        key={index}
+                        src={index < props.rating ? star : stardull} 
+                        alt="star"
+                        className='w-4 h-4' 
+                    />
+                ))}
+            </div>
+
+            {/* Review Count */}
+            <div className='text-black text-sm text-left mt-1 ml-1'>
+                ({props.reviewCount} reviews)
+            </div>
             
-            {/* <Link to='/login'>
-                <button className='bg-yellow-300 rounded-full mr-2 w-[100px] mt-2 mb-2'>Buy Now</button>
-            </Link>
-            <Link to='/cart'>
-                <button className='bg-yellow-300 rounded-full w-[120px]'>Add to Cart</button>
-            </Link> */}
         </div>
     );
 };
 
-export default Items;
+export default Item;
